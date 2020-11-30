9b150460d7bf6d80743cf456a0b01c42d4670b1a
Philip Meholm <philip@nimtech.no>
2020-11-30 03:08:54 +0100

    module can now be loaded as a part of a ps1

--- with.menu/with.menu.psm1
+++ with.menu/with.menu.psm1
@@ -1,14 +1,8 @@
-
-# gci "$PSScriptRoot\code\class" -File -Filter "*class.ps1"|%{
-#     Write-host $_.FullName
-#     . $_.FullName
-# }
-
+gci "$PSScriptRoot\code" -File -Filter "*.ps1" -Recurse | ? { $_.Directory.name -in "class" } | % {
+    . $_.FullName
+}
 gci "$PSScriptRoot\code" -File -Filter "*.ps1" -Recurse | ? { $_.Directory.name -in "public", "private" } | % {
     . $_.FullName
 }
 
-# $Global:WithMenuSettings = $null
-# $settings = [with_menu_setting]::new()
-# $settings.SetGlobal()
 $Global:_MenuSettings = @{}
